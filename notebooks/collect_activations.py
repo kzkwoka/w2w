42ef6d4c1e4ea61909864ff6fdfcd9e4b3c75327
Kinga Kwoka <plgkingak@login01.athena.cyfronet.pl>
2025-05-08 00:07:13 +0200

    Test lstsq direction edits

--- notebooks/collect_activations.py
+++ notebooks/collect_activations.py
@@ -9,7 +9,7 @@ from sae import Sae
 path = "weights2weights/weights_datasets"
 device = "cuda"
 
-sae_path = "runs/test_feature_logging/0"
+sae_path = "runs/run_lat5000_bs512_auxk0.03_k256_epoch10_1381189_0/0"
 sae = Sae.load_from_disk(sae_path,device=device)
 sae.eval()
 
@@ -21,15 +21,26 @@ dl = DataLoader(
             shuffle=False
         )
 
-activations = []
+n_samples = len(dataset)
+activations_tensor = torch.empty((n_samples, 5000), dtype=torch.float32)
+filenames = [None] * n_samples
 
+offset=0
 with torch.no_grad():
     for batch in tqdm(dl):
-        filenames = batch['filename']
         data_batch = torch.stack(batch['data'], dim=1).to(device)
 
-        output_batch = sae.pre_acts(data_batch)
-        activations.extend(zip(filenames, output_batch))
+        output_batch = sae.pre_acts(data_batch.to(device))
+        # #TODO: maybe no need to extend, rather collect differently
+        # activations.extend(zip(filenames, output_batch))
+        
+        bsz = output_batch.size(0)
 
-activations = dict(activations)
-torch.save(activations, f"{path}/activations.pt")
+        activations_tensor[offset:offset + bsz] = output_batch
+        filenames[offset:offset + bsz] = batch["filename"]
+
+        offset += bsz
+
+activations = dict(zip(filenames, activations_tensor.cpu()))
+
+torch.save(activations, f"{path}/activations_crosscoder.pt")
